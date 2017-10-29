011db498fa9e1f55af84033412062d2d9ff6246d
iykequame <zac6ix@gmail.com>
2017-10-29 20:49:22 +0000

    add test hardware files

---
+++ Android.mk
@@ -0,0 +1,11 @@
+# Lenovo aio_otfp hardwares
+# modified for 
+# Motorola E4 (woods)
+#
+# by: daniel_hk (https://github.com/danielhk)
+# modified by : zac6ix (https://github.com/iykequame)
+#
+LOCAL_PATH := $(my-dir)
+ifeq ($(TARGET_DEVICE),woods)
+include $(call first-makefiles-under,$(LOCAL_PATH))
+endif
